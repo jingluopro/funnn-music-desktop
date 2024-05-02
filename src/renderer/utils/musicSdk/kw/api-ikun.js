72f6884f8e020df8061ea67eedef882c78d5b1c9
ikun <ikun0014@qq.com>
2024-05-02 23:18:55 +0800

    Revert "fix: 无法播放"
    This reverts commit b018772605f71aeeb14ac4ee3374ffcc0be656b5.

--- src/renderer/utils/musicSdk/kw/api-ikun.js
+++ src/renderer/utils/musicSdk/kw/api-ikun.js
@@ -5,7 +5,7 @@ import { dnsLookup } from '../utils'
 
 const api_ikun = {
   getMusicUrl(songInfo, type) {
-    const requestObj = httpFetch(`https://lxmusic.ikunshare.com/url/kw/${songInfo.songmid}/${type}`, {
+    const requestObj = httpFetch(`https://lxmusic.ikunshare.com:9763/url/kw/${songInfo.songmid}/${type}`, {
       method: 'get',
       timeout,
       headers,
